1c941f2d52c315486f02fe4d18da1b4aacf906da
yanqizhao <yanqizhao@126.com>
2017-11-14 20:00:08 +0800

    nscondition

--- nscondition/nscondition/ViewController.m
+++ nscondition/nscondition/ViewController.m
@@ -17,23 +17,47 @@ static int _products;
 @implementation ViewController
 
 - (void)viewDidLoad {
-	[super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	[super viewDidLoad];	
+//	NSCondition *condition = [[NSCondition alloc] init];
+//	dispatch_async(dispatch_get_global_queue(0, 0), ^{
+//		[condition lock];
+//		NSLog(@"1%@", [NSThread currentThread]);
+//		while (_products == 0) {
+//			NSLog(@"wait for products");
+//			[condition wait];
+//		}
+//		_products -= 1;
+//		NSLog(@"consume a product");
+//		[condition unlock];
+//	});
+//	dispatch_async(dispatch_get_global_queue(0, 0), ^{
+//		[condition lock];
+//		NSLog(@"2%@", [NSThread currentThread]);
+//		_products += 1;
+//		NSLog(@"produce a product");
+//		[condition signal];
+//		[condition unlock];
+//	});
+	
 	NSCondition *condition = [[NSCondition alloc] init];
 	dispatch_async(dispatch_get_global_queue(0, 0), ^{
-		NSLog(@"1%@", [NSThread currentThread]);
 		[condition lock];
-		while (_products == 0) {
-			NSLog(@"wait for products");
-			[condition wait];
+		NSLog(@"1%@", [NSThread currentThread]);
+		NSLog(@"wait for products");
+		[condition waitUntilDate:[NSDate dateWithTimeIntervalSinceNow:1]];
+		if (_products > 0) {
+			_products -= 1;
+			NSLog(@"consume a product");
 		}
-		_products -= 1;
-		NSLog(@"consume a product");
+		else {
+			NSLog(@"timeout");
+		}
 		[condition unlock];
 	});
-	
+
 	dispatch_async(dispatch_get_global_queue(0, 0), ^{
 		NSLog(@"2%@", [NSThread currentThread]);
+		[NSThread sleepForTimeInterval:3];
 		[condition lock];
 		_products += 1;
 		NSLog(@"produce a product");
@@ -42,11 +66,9 @@ static int _products;
 	});
 }
 
-
 - (void)didReceiveMemoryWarning {
 	[super didReceiveMemoryWarning];
 	// Dispose of any resources that can be recreated.
 }
-
 
 @end
